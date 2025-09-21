19085328bedde015d1aeb373258d936f73f26d10
khangmoihocit <23a1001d0196@students.hou.edu.vn>
2025-09-21 08:24:15 +0700

    feat: Initialize RecycleView project and update HandleEvent project
    - Add RecycleView project:
        - Add MainActivity.java with basic setup and edge-to-edge display.
        - Add activity_main.xml layout with a RecyclerView.
        - Add Cat.java data model with fields for image and name.
        - Add item.xml layout for RecyclerView items, displaying an ImageView in a CardView.
        - Add drawable resources img.png and img_1.png.
    - Update HandleEvent project:
        - Remove EdgeToEdge and ViewCompat code from RecycleViewActivity.java.
        - Add IntelliJ IDEA configuration files:
            - AndroidProjectSystem.xml
            - appInsightsSettings.xml
            - compiler.xml
            - deploymentTargetSelector.xml
            - deviceManager.xml
            - gradle.xml
            - migrations.xml
            - misc.xml
            - runConfigurations.xml
            - vcs.xml

---
+++ RecycleView/app/src/main/java/com/khangmoihocit/recycleview/model/Cat.java
@@ -0,0 +1,24 @@
+package com.khangmoihocit.recycleview.model;
+
+public class Cat {
+    private int img;
+    private String name;
+
+    public Cat(){}
+
+    public int getImg() {
+        return img;
+    }
+
+    public void setImg(int img) {
+        this.img = img;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
