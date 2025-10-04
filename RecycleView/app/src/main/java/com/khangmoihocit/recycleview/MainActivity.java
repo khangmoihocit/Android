aa09d82e9a33a6b174dbec8c08318c5784b54419
khangmoihocit <23a1001d0196@students.hou.edu.vn>
2025-10-04 08:51:11 +0700

    feat: Implement RecyclerView with a grid of cats
    - Add `CatAdapter.java` to manage the display of `Cat` objects in a RecyclerView.
    - Update `MainActivity.java`:
        - Set up a RecyclerView with a `GridLayoutManager` (3 columns).
        - Create a list of `Cat` objects and pass it to the `CatAdapter`.
    - Update `Cat.java` to include a constructor that accepts an image resource and a name.
    - Modify `item.xml` layout for the RecyclerView item:
        - Add an ID to the `CardView`.
        - Add a `TextView` to display the cat's name below the `ImageView`.
    - Add new drawable resources for cat images: `img_2.png` and `img_3.png`.

--- RecycleView/app/src/main/java/com/khangmoihocit/recycleview/MainActivity.java
+++ RecycleView/app/src/main/java/com/khangmoihocit/recycleview/MainActivity.java
@@ -4,21 +4,43 @@ import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.helper.widget.Grid;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.khangmoihocit.recycleview.model.Cat;
+import com.khangmoihocit.recycleview.model.CatAdapter;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+    private RecyclerView recyclerView;
+    private CatAdapter catAdapter;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+        recyclerView = findViewById(R.id.rview);
+        catAdapter = new CatAdapter(this, getList());
+        GridLayoutManager manager = new GridLayoutManager(this, 3); //spanCount - số cột
+        recyclerView.setLayoutManager(manager);
+        recyclerView.setAdapter(catAdapter);
+
+    }
+
+    private List<Cat> getList(){
+        List<Cat> list = new ArrayList<>();
+        list.add(new Cat(R.drawable.img, "meo 1"));
+        list.add(new Cat(R.drawable.img_1, "meo 2"));
+        list.add(new Cat(R.drawable.img_2, "meo 3"));
+        list.add(new Cat(R.drawable.img_3, "meo 4"));
+        return list;
     }
 }
---
+++ RecycleView/app/src/main/java/com/khangmoihocit/recycleview/model/CatAdapter.java
@@ -0,0 +1,76 @@
+package com.khangmoihocit.recycleview.model;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.cardview.widget.CardView;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.khangmoihocit.recycleview.R;
+
+import java.util.List;
+
+//RecyclerView.Adapter<CatAdapter.CatViewHolder>: có 3 lớp phương thức cần ghi đè
+public class CatAdapter extends RecyclerView.Adapter<CatAdapter.CatViewHolder>{
+    private Context context;
+    private List<Cat> list;
+
+    public CatAdapter(Context context, List<Cat> list) {
+        this.context = context;
+        this.list = list;
+    }
+
+    @NonNull
+    @Override //trả về 1 view, tạo khung gồm 1 list các item trống
+    public CatViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        //view cho layout nao
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item, parent, false);
+        return new CatViewHolder(view);
+    }
+
+    @Override //gán dữ liệu vào các item trống trong khung theo từng position
+    //dùng để gắn (bind) dữ liệu từ một vị trí cụ thể trong danh sách của bạn
+    // vào các thành phần giao diện (views) trong một ViewHolder.
+    public void onBindViewHolder(@NonNull CatViewHolder holder, int position) {
+        Cat cat = list.get(position); //position: vị trí của item trong list
+        if(cat == null) return;
+        holder.img.setImageResource(cat.getImg());
+        holder.tv.setText(cat.getName());
+
+        //gán sự kiện click vào 1 cardview
+//        holder.cv.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Toast.makeText(context.getApplicationContext()
+//                        , cat.getName()
+//                        , Toast.LENGTH_SHORT).show();
+//            }
+//        });
+    }
+
+    @Override
+    public int getItemCount() {
+        if(list != null) return list.size();
+        return 0;
+    }
+
+    //la 1 item (layout - item.xml)
+    public class CatViewHolder extends RecyclerView.ViewHolder{
+        private ImageView img;
+        private TextView tv;
+//        private CardView cv;
+
+        public CatViewHolder(@NonNull View itemView) {
+            super(itemView);
+            img = itemView.findViewById(R.id.img);
+            tv = itemView.findViewById(R.id.tv);
+//            cv = itemView.findViewById(R.id.cvCat);
+        }
+    }
+}
