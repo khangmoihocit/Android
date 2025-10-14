7987e1d71b69b02dbf2cc784251775b465339332
khangmoihocit <23a1001d0196@students.hou.edu.vn>
2025-10-14 10:53:33 +0700

    up

---
+++ NoteApps/app/build.gradle.kts
@@ -0,0 +1,50 @@
+// File: build.gradle.kts
+
+plugins {
+    alias(libs.plugins.android.application)
+}
+
+android {
+    namespace = "com.khangmoihocit.noteapps"
+    compileSdk = 36
+
+    defaultConfig {
+        applicationId = "com.khangmoihocit.noteapps"
+        minSdk = 24
+        targetSdk = 36
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+    buildFeatures {
+        viewBinding = true
+    }
+}
+
+dependencies {
+    implementation(libs.appcompat)
+    implementation(libs.material)
+
+    implementation(libs.recyclerview)
+    implementation(libs.constraintlayout)
+    implementation(libs.activity)
+
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.ext.junit)
+    androidTestImplementation(libs.espresso.core)
+}
