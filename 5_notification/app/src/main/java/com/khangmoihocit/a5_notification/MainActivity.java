299283281b744dc7eb561d45a9b3e8b124a47967
khangmoihocit <23a1001d0196@students.hou.edu.vn>
2025-11-25 10:54:33 +0700

    up

---
+++ 5_notification/app/src/main/java/com/khangmoihocit/a5_notification/MainActivity.java
@@ -0,0 +1,66 @@
+package com.khangmoihocit.a5_notification;
+
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.content.Context;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import java.util.Date;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final int NOTIFICATION_ID = 1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        Button btnSend = findViewById(R.id.btnSendNotification);
+        btnSend.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                sendNotification();
+            }
+        });
+    }
+
+    //add permission in android mainfest
+    private void sendNotification() {
+        Notification notification = new NotificationCompat.Builder(this, MyApplication.CHANNEL_ID)
+                .setContentTitle("Title push notification")
+                .setContentText("Message push notification")
+                .setSmallIcon(R.drawable.ic_launcher_background)
+                .build();
+
+        NotificationManager notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        if(notificationManager != null){
+            notificationManager.notify(getNotificationId(), notification);
+        }
+
+        //other way
+//        NotificationManagerCompat notificationManagerCompat = NotificationManagerCompat.from(this);
+//        notificationManagerCompat.notify(getNotificationId(), notification);
+
+    }
+
+    private int getNotificationId(){
+        return (int) new Date().getTime();
+    }
+}
